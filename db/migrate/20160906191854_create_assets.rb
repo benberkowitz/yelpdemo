0b639896a40466a182f9f3aa3064bf6652ff2407
Ben Berkowitz <ben@seeclickfix.com>
2016-09-07 11:49:59 -0400

    first commit

---
+++ db/migrate/20160906191854_create_assets.rb
@@ -0,0 +1,11 @@
+class CreateAssets < ActiveRecord::Migration[5.0]
+  def change
+    create_table :assets do |t|
+      t.string :name
+      t.string :address
+      t.string :assetType
+
+      t.timestamps
+    end
+  end
+end
